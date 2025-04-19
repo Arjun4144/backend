bf9f1c9ba42a907bc493f5307930c6225ee40805
Arjun Rawat <arjunrawat4144@gmail.com>
2025-04-19 15:54:09 +0530

    some addition to practice

--- src/index.js
+++ src/index.js
@@ -8,7 +8,9 @@ import { app } from './app.js';
 import dotenv from 'dotenv';
 import connectDB from './db/index.js';
 
-dotenv.config({ path: '../.env' });
+dotenv.config({ 
+    path: '../.env' 
+});
 
 
 
